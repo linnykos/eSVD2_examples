5501c59c0c3d250430d284afd366f12755838416
Kevin Lin <klin1234@gmail.com>
2021-04-28 16:49:03 -0400

    Trying different methods

---
+++ examples/examples_different_methods.R
@@ -0,0 +1,74 @@
+rm(list=ls())
+set.seed(202)
+ngenes <- 5000 #must be divisible by 10
+ngenes_informative <- ngenes*.1
+ncells <- 50 #number of cells per cluster, must be divisible by 2
+nclust<- 3
+# simulate two batches with different depths
+batch <- rep(1:2, each = nclust*ncells/2)
+ncounts <- stats::rpois(ncells*nclust, lambda = 1000*batch)
+# generate profiles for 3 clusters
+profiles_informative <- replicate(nclust, exp(stats::rnorm(ngenes_informative)))
+profiles_const <- matrix(ncol=nclust, rep(exp(stats::rnorm(ngenes-ngenes_informative)), nclust))
+profiles <- rbind(profiles_informative,profiles_const)
+# generate cluster labels
+clust <- sample(rep(1:3, each = ncells))
+# generate single-cell transcriptomes
+counts <- sapply(seq_along(clust), function(i){
+  stats::rmultinom(1, ncounts[i], prob = profiles[,clust[i]])
+})
+rownames(counts) <- paste("gene", seq(nrow(counts)), sep = "_")
+colnames(counts) <- paste("cell", seq(ncol(counts)), sep = "_")
+# clean up rows
+dat <- t(counts[rowSums(counts) > 0, ])
+
+########################
+
+# try just PCA
+pca_res <- stats::prcomp(dat)
+plot(pca_res$x[,1], pca_res$x[,2], asp = T, col = clust, pch = 16)
+
+# try UMAP
+dat2 <- scale(dat, center = T, scale = T)
+svd_res <- RSpectra::svds(dat2, k = 10)
+dim_red <- .mult_mat_vec(svd_res$u, svd_res$d)
+set.seed(10)
+umap_res <- Seurat::RunUMAP(dim_red)@cell.embeddings
+plot(umap_res[,1], umap_res[,2], asp = T, col = clust, pch = 16)
+
+# try glm-pca
+set.seed(10)
+glm_res <- glmpca::glmpca(t(dat), 2, fam="poi")
+plot(glm_res$factors[,1], glm_res$factors[,2], asp = T, col = clust, pch = 16)
+plot(glm_res$loadings[,1], glm_res$loadings[,2], asp = T, pch = 16)
+plot(glm_res$dev)
+plot(glm_res$coefX[,1])
+
+# try ZINB-WaVE
+# set.seed(10)
+# dat_se <- SummarizedExperiment::SummarizedExperiment(assays = list(counts = t(dat)))
+# tmp <- zinbwave::zinbwave(dat_se, K = 2, maxiter.optimize = 100, normalizedValues = F,
+#                           verbose = T, commondispersion = F, BPPARAM = BiocParallel::SerialParam())
+# fit <- SingleCellExperiment::reducedDims(tmp)$zinbwave
+
+# try pcmf
+set.seed(10)
+pcmf_res <- pCMF::pCMF(dat, K = 2, sparsity = F, verbose = T, ncores = 1)
+plot(pcmf_res$factor$U[,1], pcmf_res$factor$U[,2], asp = T, col = clust, pch = 16)
+
+# try eSVD
+set.seed(10)
+library_size_vec <- rowSums(dat)
+init <- eSVD2::initialize_esvd(dat, k = 3, family = "poisson", nuisance_param_vec = NA,
+                               library_size_vec = library_size_vec,
+                               config = eSVD2::initialization_options())
+esvd_res <- opt_esvd(init$x_mat, init$y_mat, dat, family = "poisson",
+                nuisance_param_vec = NA, library_size_vec = library_size_vec,
+                max_iter = 25, verbose = 1)
+plot(esvd_res$x_mat[,1], esvd_res$x_mat[,2], asp = T, col = clust, pch = 16)
+plot(esvd_res$loss)
+set.seed(10)
+umap_res <- Seurat::RunUMAP(esvd_res$x)@cell.embeddings
+plot(umap_res[,1], umap_res[,2], asp = T, col = clust, pch = 16)
+
+
