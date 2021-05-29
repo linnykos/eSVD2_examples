9776ed3905e2ba9bcbd0785e2253c274350da5c5
Kevin Lin <klin1234@gmail.com>
2021-05-29 12:34:29 -0400

    Changing the zinbwave shell

--- experiment/writeup6/writeup6_citeseq_bm_zinbwave.sh
+++ experiment/writeup6/writeup6_citeseq_bm_zinbwave.sh
@@ -2,6 +2,7 @@
 #$ -N bm_zinbwave
 #$ -j y
 #$ -o ../../../../out/writeup6/qsub/
+#$ -l m_mem_free=12G
 #$ -pe openmpi 4
 
 Rscript --no-save writeup6_citeseq_bm_zinbwave.R
---
+++ experiment/writeup6/writeup6_citeseq_bm_zinbwave_postprocess.R
@@ -0,0 +1,2 @@
+rm(list=ls())
+load("../../../../out/writeup6/writeup6_citeseq_bm_zinbwave.RData")
