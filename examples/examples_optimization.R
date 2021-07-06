474da675abb8f85927daa625bd3dcd3b5c4c4a3d
Kevin Lin <klin1234@gmail.com>
2021-07-05 20:38:39 -0400

    Updates

--- examples/examples_optimization.R
+++ examples/examples_optimization.R
@@ -25,31 +25,29 @@ res <- opt_esvd(init$x_mat, init$y_mat, dat, family = "curved_gaussian",
 nat_est <- tcrossprod(res$x, res$y)
 plot(res$loss)
 
-
-
-# Exponential example
-library(eSVD2)
-set.seed(123)
-n <- 100
-p <- 150
-k <- 5
-x_mat <- matrix(abs(rnorm(n * k)), nrow = n, ncol = k)
-y_mat <- matrix(abs(rnorm(p * k)), nrow = p, ncol = k)
-nat_mat <- -tcrossprod(x_mat, y_mat) / 10
-
-## Simulate data
-dat <- eSVD2::generate_data(nat_mat, family = "exponential", nuisance_param_vec = NA,
-                            library_size_vec = 1)
-
-## Determine initialization
-init <- eSVD2::initialize_esvd(
-    dat, k = k, family = "exponential", nuisance_param_vec = NA, library_size_vec = 1,
-    config = eSVD2::initialization_options()
-)
-
-## Optimization
-res <- opt_esvd(init$x_mat, init$y_mat, dat, family = "exponential",
-                nuisance_param_vec = NA, library_size_vec = 1,
-                max_iter = 15, verbose = 1)
-nat_est <- tcrossprod(res$x, res$y)
-plot(res$loss)
+# # Exponential example
+# library(eSVD2)
+# set.seed(123)
+# n <- 100
+# p <- 150
+# k <- 5
+# x_mat <- matrix(abs(rnorm(n * k)), nrow = n, ncol = k)
+# y_mat <- matrix(abs(rnorm(p * k)), nrow = p, ncol = k)
+# nat_mat <- -tcrossprod(x_mat, y_mat) / 10
+#
+# ## Simulate data
+# dat <- eSVD2::generate_data(nat_mat, family = "exponential", nuisance_param_vec = NA,
+#                             library_size_vec = 1)
+#
+# ## Determine initialization
+# init <- eSVD2::initialize_esvd(
+#     dat, k = k, family = "exponential", nuisance_param_vec = NA, library_size_vec = 1,
+#     config = eSVD2::initialization_options()
+# )
+#
+# ## Optimization
+# res <- opt_esvd(init$x_mat, init$y_mat, dat, family = "exponential",
+#                 nuisance_param_vec = NA, library_size_vec = 1,
+#                 max_iter = 15, verbose = 1)
+# nat_est <- tcrossprod(res$x, res$y)
+# plot(res$loss)
--- experiment/experiment.R
+++ experiment/experiment.R
@@ -1,34 +1,69 @@
 rm(list=ls())
-set.seed(20)
-cov_x <- cov(MASS::mvrnorm(n = 10, rep(0, 5), diag(5)))
-cov_y <- cov(MASS::mvrnorm(n = 10, rep(0, 5), toeplitz(5:1)))
+set.seed(5)
+canon_vec <- 1:50
+nat_mat <- sapply(log(canon_vec)+1, function(x){rep(x, 100)})
+dat <- generate_data(nat_mat, family = "poisson")
 
-res <- .identification(cov_x, cov_y, check = T)
+nuisance_param_vec = NA
+k = 2
+family = "poisson"
+library_size_vec = NA
+covariates = matrix(1, nrow = nrow(dat), ncol = 1)
+config = initialization_options()
+verbose = 0
 
-##############
-check = T; tol = 1e-6
+#####
 
-eigen_x <- eigen(cov_x)
-eigen_y <- eigen(cov_y)
-
-Vx <- eigen_x$vectors
-Vy <- eigen_y$vectors
-
-if(any(eigen_x$values <= tol) | any(eigen_y$values <= tol)) warning("Detecting rank defficiency in reparameterization step")
-
-Dx <- eigen_x$values
-Dy <- eigen_y$values
-
-# form R
-tmp2 <- diag(sqrt(Dy)) %*% t(Vy) %*% Vx %*% diag(sqrt(Dx))
-tmp <- crossprod(.mult_mat_vec(Vy, sqrt(Dy)), .mult_mat_vec(Vx, sqrt(Dx)))
-stopifnot(sum(abs(tmp2 - tmp)) <= 1e-6)
-svd_tmp <- svd(tmp)
-Q <- tcrossprod(svd_tmp$u, svd_tmp$v)
-
-# run a check
-if(check){
-  sym_mat <- crossprod(Q, tmp)
-
-  stopifnot(sum(abs(sym_mat - t(sym_mat))) <= 1e-6)
+stopifnot(is.character(family))
+if(family != "gaussian") stopifnot(all(dat[!is.na(dat)] >= 0))
+if(all(!is.na(nuisance_param_vec)) & length(nuisance_param_vec) == 1) {
+  nuisance_param_vec <- rep(nuisance_param_vec[1], ncol(dat))
 }
+
+# estimate library sizes if asked
+n <- nrow(dat); p <- ncol(dat)
+family <- .string_to_distr_funcs(family)
+library_size_vec <- .parse_library_size(dat, library_size_vec = library_size_vec)
+rescaled_dat <- t(sapply(1:nrow(dat), function(i){
+  dat[i,]/library_size_vec[i]
+}))
+
+# determine initial matrix taking into account to missing values and library size
+# [note to self: this probably could be something a lot simpler]
+rescaled_dat <- .matrix_completion(rescaled_dat, k = k)
+init_res <- .determine_initial_matrix(rescaled_dat, k = k, family = family,
+                                      nuisance_param_vec = nuisance_param_vec,
+                                      max_val = config$max_val,
+                                      tol = config$tol)
+nat_mat <- init_res$nat_mat; domain <- init_res$domain
+
+if(all(is.null(covariates)))
+{
+  b_mat <- NULL
+} else {
+  r <- ncol(covariates)
+  # do regression
+  tmp <- lapply(1:p, function(j){
+    .regress_covariates(nat_mat[,j], covariates)
+  })
+
+  nat_mat <- sapply(1:p, function(j){tmp[[j]]$residual})
+  b_mat <- sapply(1:p, function(j){tmp[[j]]$coef})
+  baseline <- tcrossprod(covariates, b_mat)
+}
+
+# project inital matrix into space of low-rank matrices
+nat_mat2 <- .initialize_nat_mat(nat_mat, k = k, baseline = baseline,
+                               domain = domain, config = config)
+
+Matrix::rankMatrix(nat_mat2)
+quantile(nat_mat)
+# res <- .factorize_matrix(nat_mat, k = k, equal_covariance = T)
+
+
+###########3
+
+svd_res <- .svd_truncated(nat_mat, K = k, symmetric = F, rescale  = F,
+                          mean_vec = NULL, sd_vec = NULL, K_full_rank = F)
+x_mat <- .mult_mat_vec(svd_res$u, sqrt(svd_res$d))
+y_mat <- .mult_mat_vec(svd_res$v, sqrt(svd_res$d))
