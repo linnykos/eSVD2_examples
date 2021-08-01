aab47ea848a626ff20b101a82f6f7433b1a05d21
Kevin Lin <klin1234@gmail.com>
2021-08-01 12:27:33 -0400

    Fixing the bugs

--- experiment/writeup6b/writeup6_10x_mousepancreas_esvd.R
+++ experiment/writeup6b/writeup6_10x_mousepancreas_esvd.R
@@ -6,6 +6,7 @@ session_info <- devtools::session_info()
 print("Loading in data")
 dat <- anndata::read_h5ad("../../../../data/10x_mousepancreas/endocrinogenesis_day15.5.h5ad")
 tmp <- Matrix::t(dat$X)
+tmp <- Matrix::Matrix(as.matrix(tmp), sparse = T)
 clusters <- dat$obs$clusters
 rm(list = "dat")
 gc()
@@ -18,7 +19,8 @@ pancreas <-  Seurat::FindVariableFeatures(pancreas, selection.method = "vst", nf
 
 # extract data
 mat <- pancreas[["RNA"]]@counts[Seurat::VariableFeatures(pancreas),]
-mat <- t(mat)
+mat <- Matrix::t(mat)
+mat <- as.matrix(mat)
 
 # run eSVD
 set.seed(10)
@@ -30,7 +32,7 @@ init <- eSVD2::initialize_esvd(mat, k = K, family = "poisson", nuisance_param_ve
                                library_size_vec = 1,
                                covariates = covariates,
                                config = eSVD2::initialization_options(), verbose = 1)
-esvd_res <- eSVD2::opt_esvd(init$x_mat, init$y_mat, dat, family = "poisson",
+esvd_res <- eSVD2::opt_esvd(init$x_mat, init$y_mat, mat, family = "poisson",
                      nuisance_param_vec = NA, library_size_vec = 1,
                      b_init = init$b_mat, covariates = covariates,
                      max_iter = 50, verbose = 1)
