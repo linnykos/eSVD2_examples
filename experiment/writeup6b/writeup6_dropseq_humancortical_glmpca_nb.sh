36bbc7cd0afefb7a90157dc6e3b6d6eeba6e8752
Kevin Lin <klin1234@gmail.com>
2021-08-02 00:04:30 -0400

    Fixed typo

--- experiment/writeup6b/writeup6_dropseq_humancortical_glmpca_nb.sh
+++ experiment/writeup6b/writeup6_dropseq_humancortical_glmpca_nb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #$ -N cortical_glmpca
 #$ -j y
-#$ -o ../../../../out/kevin/Writeup3b/qsub
+#$ -o ../../../../out/writeup6b/qsub/
 #$ -l m_mem_free=50G
 
 Rscript --no-save writeup6_dropseq_humancortical_glmpca_nb.R
