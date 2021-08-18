b1ac3d76554e62fedc1cd764ec5014e490651b9e
linnykos <klin1234@gmail.com>
2021-08-17 23:25:35 -0400

    Pushing covariates for eSVD

--- experiment/writeup7/writeup7_sns_esvd_covariates.R
+++ experiment/writeup7/writeup7_sns_esvd_covariates.R
@@ -12,6 +12,26 @@ n <- nrow(mat)
 library_size_vec <- rowSums(mat)
 covariates <- cbind(matrix(1, nrow = n, ncol = 1), log(library_size_vec))
 
+uniq_indiv <- unique(sns@meta.data$individual)
+uniq_diagnos <- unique(sns@meta.data$diagnosis)
+uniq_sex <- unique(sns@meta.data$sex)
+for(i in uniq_indiv){
+  tmp <- rep(0, n)
+  tmp[which(sns@meta.data$individual == i)] <- 1
+  covariates <- cbind(covariates, tmp)
+}
+for(i in uniq_diagnos){
+  tmp <- rep(0, n)
+  tmp[which(sns@meta.data$diagnosis == i)] <- 1
+  covariates <- cbind(covariates, tmp)
+}
+for(i in uniq_sex){
+  tmp <- rep(0, n)
+  tmp[which(sns@meta.data$sex == i)] <- 1
+  covariates <- cbind(covariates, tmp)
+}
+colnames(covariates) <- c("Intercept", "Log-library", uniq_indiv, uniq_diagnos, uniq_sex)
+
 K <- 30
 print("Starting initialization")
 init <- eSVD2::initialize_esvd(mat, k = K, family = "poisson", nuisance_param_vec = NA,
@@ -24,4 +44,4 @@ esvd_res <- eSVD2::opt_esvd(init$x_mat, init$y_mat, mat, family = "poisson",
                             b_init = init$b_mat, covariates = covariates,
                             max_iter = 100, verbose = 1)
 
-save.image("../../../../out/writeup7/writeup7_sns_esvd.RData")
+save.image("../../../../out/writeup7/writeup7_sns_esvd_covariates.RData")
