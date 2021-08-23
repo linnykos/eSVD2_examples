e5f9fa24520860a0f348f81f1b6feaa111aec922
linnykos <klin1234@gmail.com>
2021-08-23 09:39:05 -0400

    eSVD covariates not having sex now

--- experiment/writeup7/writeup7_sns_esvd_covariates_large.R
+++ experiment/writeup7/writeup7_sns_esvd_covariates_large.R
@@ -51,34 +51,39 @@ n <- nrow(mat)
 library_size_vec <- rowSums(mat)
 covariates <- cbind(matrix(1, nrow = n, ncol = 1), log(library_size_vec))
 uniq_diagnos <- unique(sns@meta.data$diagnosis)
-uniq_sex <- unique(sns@meta.data$sex)
+# uniq_sex <- unique(sns@meta.data$sex)
 for(i in uniq_diagnos[-1]){
   tmp <- rep(0, n)
   tmp[which(sns@meta.data$diagnosis == i)] <- 1
   covariates <- cbind(covariates, tmp)
 }
-for(i in uniq_sex[-1]){
-  tmp <- rep(0, n)
-  tmp[which(sns@meta.data$sex == i)] <- 1
-  covariates <- cbind(covariates, tmp)
-}
-colnames(covariates) <- c("Intercept", "Log-library", uniq_diagnos[-1], uniq_sex[-1])
+# for(i in uniq_sex[-1]){
+#   tmp <- rep(0, n)
+#   tmp[which(sns@meta.data$sex == i)] <- 1
+#   covariates <- cbind(covariates, tmp)
+# }
+# colnames(covariates) <- c("Intercept", "Log-library", uniq_diagnos[-1], uniq_sex[-1])
+colnames(covariates) <- c("Intercept", "Log-library", uniq_diagnos[-1])
 
 K <- 30
 print("Starting initialization")
+time_start1 <- Sys.time()
 set.seed(10)
 init <- eSVD2::initialize_esvd(mat, k = K, family = "poisson", nuisance_param_vec = NA,
                                library_size_vec = 1,
                                covariates = covariates,
                                config = eSVD2::initialization_options(), verbose = 1)
+time_end1 <- Sys.time()
 save.image("../../../../out/writeup7/writeup7_sns_esvd_covariates_large.RData")
 
 print("Starting estimation")
+time_start2 <- Sys.time()
 set.seed(10)
 esvd_res <- eSVD2::opt_esvd(init$x_mat, init$y_mat, mat, family = "poisson",
                             nuisance_param_vec = NA, library_size_vec = 1,
                             b_init = init$b_mat, covariates = covariates,
                             max_iter = 100, verbose = 1)
+time_end2 <- Sys.time()
 
 save.image("../../../../out/writeup7/writeup7_sns_esvd_covariates_large.RData")
 
