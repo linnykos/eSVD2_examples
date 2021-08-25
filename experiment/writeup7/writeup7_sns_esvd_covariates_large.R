37d03c94a4985749f81b609ba58486b6823e9771
linnykos <klin1234@gmail.com>
2021-08-25 10:05:10 -0400

    Fixing a bug in the eSVD covariates

--- experiment/writeup7/writeup7_sns_esvd_covariates_large.R
+++ experiment/writeup7/writeup7_sns_esvd_covariates_large.R
@@ -6,40 +6,39 @@ session_info <- devtools::session_info()
 
 library(Seurat)
 
-load("../../../../data/sns_autism/raw_counts_mat.RData")
-metadata <- read.csv("../../../../data/sns_autism/meta.txt", sep = "\t", header = T)
-
-##########
-
-print("Preprocessing")
-sns <- Seurat::CreateSeuratObject(counts = mat)
-sns[["percent.mt"]] <- Seurat::PercentageFeatureSet(sns, pattern = "^MT-")
-
-# the following set is already satisfied
-# sns <- subset(sns, subset = nFeature_RNA > 500 & percent.mt < 5)
-
-set.seed(10)
-Seurat::DefaultAssay(sns) <- "RNA"
-sns <- Seurat::SCTransform(sns, verbose = T, variable.features.n = 50000)
-sns <- Seurat::RunPCA(sns, verbose = F)
-set.seed(10)
-sns <- Seurat::RunUMAP(sns, dims = 1:50, reduction.name = 'umap.rna', reduction.key = 'rnaUMAP_')
-
-sns[["celltype"]] <- metadata$cluster
-sns[["sample"]] <- metadata$sample
-sns[["individual"]] <- metadata$individual
-sns[["region"]] <- metadata$region
-sns[["age"]] <- metadata$age
-sns[["sex"]] <- metadata$sex
-sns[["RNA.Integrity.Number"]] <- metadata$RNA.Integrity.Number
-sns[["post.mortem.hours"]] <- metadata$post.mortem.interval..hours.
-sns[["diagnosis"]] <- metadata$diagnosis
-sns[["Capbatch"]] <- metadata$Capbatch
-sns[["Seqbatch"]] <- metadata$Seqbatch
-rm(list = "metadata")
-save.image("../../../../out/writeup7/writeup7_sns_esvd_covariates_large.RData")
+# load("../../../../data/sns_autism/raw_counts_mat.RData")
+# metadata <- read.csv("../../../../data/sns_autism/meta.txt", sep = "\t", header = T)
+#
+# print("Preprocessing")
+# sns <- Seurat::CreateSeuratObject(counts = mat)
+# sns[["percent.mt"]] <- Seurat::PercentageFeatureSet(sns, pattern = "^MT-")
+#
+# # the following set is already satisfied
+# # sns <- subset(sns, subset = nFeature_RNA > 500 & percent.mt < 5)
+#
+# set.seed(10)
+# Seurat::DefaultAssay(sns) <- "RNA"
+# sns <- Seurat::SCTransform(sns, verbose = T, variable.features.n = 50000)
+# sns <- Seurat::RunPCA(sns, verbose = F)
+# set.seed(10)
+# sns <- Seurat::RunUMAP(sns, dims = 1:50, reduction.name = 'umap.rna', reduction.key = 'rnaUMAP_')
+#
+# sns[["celltype"]] <- metadata$cluster
+# sns[["sample"]] <- metadata$sample
+# sns[["individual"]] <- metadata$individual
+# sns[["region"]] <- metadata$region
+# sns[["age"]] <- metadata$age
+# sns[["sex"]] <- metadata$sex
+# sns[["RNA.Integrity.Number"]] <- metadata$RNA.Integrity.Number
+# sns[["post.mortem.hours"]] <- metadata$post.mortem.interval..hours.
+# sns[["diagnosis"]] <- metadata$diagnosis
+# sns[["Capbatch"]] <- metadata$Capbatch
+# sns[["Seqbatch"]] <- metadata$Seqbatch
+# rm(list = "metadata")
+# save.image("../../../../out/writeup7/writeup7_sns_esvd_covariates_large.RData")
 
 ######################
+load("../../../../out/writeup7/writeup7_sns_esvd_covariates_large.RData")
 
 print("Forming covariates")
 mat <- sns[["RNA"]]@counts[Seurat::VariableFeatures(sns), which(sns@meta.data$celltype == "L2/3")]
@@ -50,11 +49,12 @@ set.seed(10)
 n <- nrow(mat)
 library_size_vec <- rowSums(mat)
 covariates <- cbind(matrix(1, nrow = n, ncol = 1), log(library_size_vec))
-uniq_diagnos <- unique(sns@meta.data$diagnosis[which(sns@meta.data$celltype == "L2/3")])
-# uniq_sex <- unique(sns@meta.data$sex)
+metadata <- sns@meta.data
+metadata <- metadata[which(metadata$celltype == "L2/3"),]
+uniq_diagnos <- unique(metadata$diagnosis)
 for(i in uniq_diagnos[-1]){
   tmp <- rep(0, n)
-  tmp[which(sns@meta.data$diagnosis == i)] <- 1
+  tmp[which(metadata$diagnosis == i)] <- 1
   covariates <- cbind(covariates, tmp)
 }
 # for(i in uniq_sex[-1]){
