ba0a11ec30afcccfa043148629b54cada3635e0b
linnykos <klin1234@gmail.com>
2022-03-18 16:21:33 -0400

    Opening more memory

--- experiment/Writeup11/Writeup11_habermann_preprocess_step1.sh
+++ experiment/Writeup11/Writeup11_habermann_preprocess_step1.sh
@@ -2,6 +2,6 @@
 #$ -N habermann_preprocess_step1
 #$ -j y
 #$ -o ../../../../out/Writeup11/qsub/
-#$ -l m_mem_free=100G
+#$ -l m_mem_free=200G
 
 Rscript --no-save Writeup11_habermann_preprocess_step1.R
