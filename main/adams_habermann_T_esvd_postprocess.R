c1b19da515e37c13775121267a6b861dfbb4232e
linnykos <klin1234@gmail.com>
2022-07-23 14:41:13 -0400

    Pushing updated Analysis of all the other regev datasets

--- main/adams_habermann_T_esvd_postprocess.R
+++ main/adams_habermann_T_esvd_postprocess.R
@@ -22,12 +22,21 @@ habermann_df_genes_others <- unique(unlist(lapply(file_vec, function(file_suffix
                      sep = ",")
   df_mat$X
 })))
+habermann_df_genes <- setdiff(habermann_df_genes, adams_df_genes)
 de_genes <- unique(c(adams_df_genes, habermann_df_genes))
+de_genes_others <- unique(c(adams_df_genes_others, habermann_df_genes_others))
 cycling_genes <- c(cc.genes$s.genes, cc.genes$g2m.genes)
+hk_genes <- read.csv("../../../data/housekeeping/housekeeping.txt", header = F)[,1]
+de_genes_others <- setdiff(de_genes_others, de_genes)
+cycling_genes <- setdiff(cycling_genes, c(de_genes_others, de_genes))
+hk_genes <- setdiff(hk_genes, c(cycling_genes, de_genes_others, de_genes))
 
 gene_names <- names(eSVD_obj$teststat_vec)
+adam_idx <- which(gene_names %in% adams_df_genes)
+habermann_idx <- which(gene_names %in% habermann_df_genes)
+de_other_idx <- which(gene_names %in% de_genes_others)
 cycling_idx <- which(gene_names %in% cycling_genes)
-de_idx <- which(gene_names %in% de_genes)
+hk_idx <- which(gene_names %in% hk_genes)
 
 xlim <- quantile(eSVD_obj_adams$teststat_vec, probs = c(0.001, 0.999))
 ylim <- quantile(eSVD_obj_habermann$teststat_vec, probs = c(0.001, 0.999))
@@ -35,9 +44,9 @@ mean_x <- mean(eSVD_obj_adams$teststat_vec)
 mean_y <- mean(eSVD_obj_habermann$teststat_vec)
 
 png(paste0("../../../out/fig/main/adams_habermann_T_teststatistic.png"),
-    height = 1500, width = 3500,
+    height = 3000, width = 4000,
     units = "px", res = 300)
-par(mfrow = c(1,3))
+par(mfrow = c(2,3))
 plot(x = eSVD_obj_adams$teststat_vec,
      y = eSVD_obj_habermann$teststat_vec,
      xlab = "Adams", ylab = "Habermann", pch = 16, col = rgb(0.5, 0.5, 0.5, 0.1),
@@ -48,26 +57,54 @@ lines(rep(0,2), c(-100,100), col = 2, lwd = 2, lty = 2)
 lines(c(-100,100), rep(mean_y,2), col = 1, lwd = 2, lty = 2)
 lines(rep(mean_x,2), c(-100,100), col = 1, lwd = 2, lty = 2)
 
+plot(x = eSVD_obj_adams$teststat_vec[gene_names[adam_idx]],
+     y = eSVD_obj_habermann$teststat_vec[gene_names[adam_idx]],
+     xlab = "Adams", ylab = "Habermann", pch = 16, col = 1,
+     main = "Adams genes", asp = T,
+     xlim = xlim, ylim = ylim)
+lines(c(-100,100), rep(0,2), col = 2, lwd = 2, lty = 2)
+lines(rep(0,2), c(-100,100), col = 2, lwd = 2, lty = 2)
+lines(c(-100,100), rep(mean_y,2), col = 1, lwd = 2, lty = 2)
+lines(rep(mean_x,2), c(-100,100), col = 1, lwd = 2, lty = 2)
+
+plot(x = eSVD_obj_adams$teststat_vec[gene_names[habermann_idx]],
+     y = eSVD_obj_habermann$teststat_vec[gene_names[habermann_idx]],
+     xlab = "Adams", ylab = "Habermann", pch = 16, col = 2,
+     main = "Habermann genes", asp = T,
+     xlim = xlim, ylim = ylim)
+lines(c(-100,100), rep(0,2), col = 2, lwd = 2, lty = 2)
+lines(rep(0,2), c(-100,100), col = 2, lwd = 2, lty = 2)
+lines(c(-100,100), rep(mean_y,2), col = 1, lwd = 2, lty = 2)
+lines(rep(mean_x,2), c(-100,100), col = 1, lwd = 2, lty = 2)
+
+plot(x = eSVD_obj_adams$teststat_vec[gene_names[de_other_idx]],
+     y = eSVD_obj_habermann$teststat_vec[gene_names[de_other_idx]],
+     xlab = "Adams", ylab = "Habermann", pch = 16, col = 4,
+     main = "DE other genes", asp = T,
+     xlim = xlim, ylim = ylim)
+lines(c(-100,100), rep(0,2), col = 2, lwd = 2, lty = 2)
+lines(rep(0,2), c(-100,100), col = 2, lwd = 2, lty = 2)
+lines(c(-100,100), rep(mean_y,2), col = 1, lwd = 2, lty = 2)
+lines(rep(mean_x,2), c(-100,100), col = 1, lwd = 2, lty = 2)
+
+plot(x = eSVD_obj_adams$teststat_vec[gene_names[hk_idx]],
+     y = eSVD_obj_habermann$teststat_vec[gene_names[hk_idx]],
+     xlab = "Adams", ylab = "Habermann", pch = 16, col = 3,
+     main = "HK genes", asp = T,
+     xlim = xlim, ylim = ylim)
+lines(c(-100,100), rep(0,2), col = 2, lwd = 2, lty = 2)
+lines(rep(0,2), c(-100,100), col = 2, lwd = 2, lty = 2)
+lines(c(-100,100), rep(mean_y,2), col = 1, lwd = 2, lty = 2)
+lines(rep(mean_x,2), c(-100,100), col = 1, lwd = 2, lty = 2)
+
 plot(x = eSVD_obj_adams$teststat_vec[gene_names[cycling_idx]],
      y = eSVD_obj_habermann$teststat_vec[gene_names[cycling_idx]],
-     xlab = "Adams", ylab = "Habermann", pch = 16, col = 3,
+     xlab = "Adams", ylab = "Habermann", pch = 16, col = 5,
      main = "Cycling genes", asp = T,
      xlim = xlim, ylim = ylim)
 lines(c(-100,100), rep(0,2), col = 2, lwd = 2, lty = 2)
 lines(rep(0,2), c(-100,100), col = 2, lwd = 2, lty = 2)
 lines(c(-100,100), rep(mean_y,2), col = 1, lwd = 2, lty = 2)
-lines(rep(mean_x,2), c(-100,100), col = 1, lwd = 2, lty = 2)
-
-plot(x = eSVD_obj_adams$teststat_vec[gene_names[de_idx]],
-     y = eSVD_obj_habermann$teststat_vec[gene_names[de_idx]],
-     xlab = "Adams", ylab = "Habermann", pch = 16, col = 2,
-     main = "DE genes", asp = T,
-     xlim = xlim, ylim = ylim)
-lines(c(-100,100), rep(0,2), col = 2, lwd = 2, lty = 2)
-lines(rep(0,2), c(-100,100), col = 2, lwd = 2, lty = 2)
-lines(c(-100,100), rep(mean_y,2), col = 1, lwd = 2, lty = 2)
-lines(rep(mean_x,2), c(-100,100), col = 1, lwd = 2, lty = 2)
-
 graphics.off()
 
 #########################################
@@ -78,9 +115,9 @@ mean_x <- mean(eSVD_obj_adams$fit_Second$z_mat[,"Disease_Identity_IPF"])
 mean_y <- mean(eSVD_obj_habermann$fit_Second$z_mat[,"Diagnosis_IPF"])
 
 png(paste0("../../../out/fig/main/adams_habermann_T_coefficient.png"),
-    height = 1500, width = 3500,
+    height = 3000, width = 4000,
     units = "px", res = 300)
-par(mfrow = c(1,3))
+par(mfrow = c(2,3))
 plot(x = eSVD_obj_adams$fit_Second$z_mat[,"Disease_Identity_IPF"],
      y = eSVD_obj_habermann$fit_Second$z_mat[,"Diagnosis_IPF"],
      xlab = "Adams", ylab = "Habermann", pch = 16, col = rgb(0.5, 0.5, 0.5, 0.1),
@@ -91,20 +128,50 @@ lines(rep(0,2), c(-100,100), col = 2, lwd = 2, lty = 2)
 lines(c(-100,100), rep(mean_y,2), col = 1, lwd = 2, lty = 2)
 lines(rep(mean_x,2), c(-100,100), col = 1, lwd = 2, lty = 2)
 
-plot(x = eSVD_obj_adams$fit_Second$z_mat[,"Disease_Identity_IPF"][gene_names[cycling_idx]],
-     y = eSVD_obj_habermann$fit_Second$z_mat[,"Diagnosis_IPF"][gene_names[cycling_idx]],
-     xlab = "Adams", ylab = "Habermann", pch = 16, col = 3,
-     main = "Cycling genes", asp = T,
+plot(x = eSVD_obj_adams$fit_Second$z_mat[,"Disease_Identity_IPF"][gene_names[adam_idx]],
+     y = eSVD_obj_habermann$fit_Second$z_mat[,"Diagnosis_IPF"][gene_names[adam_idx]],
+     xlab = "Adams", ylab = "Habermann", pch = 16, col = 1,
+     main = "Adams genes", asp = T,
      xlim = xlim, ylim = ylim)
 lines(c(-100,100), rep(0,2), col = 2, lwd = 2, lty = 2)
 lines(rep(0,2), c(-100,100), col = 2, lwd = 2, lty = 2)
 lines(c(-100,100), rep(mean_y,2), col = 1, lwd = 2, lty = 2)
 lines(rep(mean_x,2), c(-100,100), col = 1, lwd = 2, lty = 2)
 
-plot(x = eSVD_obj_adams$fit_Second$z_mat[,"Disease_Identity_IPF"][gene_names[de_idx]],
-     y = eSVD_obj_habermann$fit_Second$z_mat[,"Diagnosis_IPF"][gene_names[de_idx]],
+plot(x = eSVD_obj_adams$fit_Second$z_mat[,"Disease_Identity_IPF"][gene_names[habermann_idx]],
+     y = eSVD_obj_habermann$fit_Second$z_mat[,"Diagnosis_IPF"][gene_names[habermann_idx]],
      xlab = "Adams", ylab = "Habermann", pch = 16, col = 2,
-     main = "DE genes", asp = T,
+     main = "Habermann genes", asp = T,
+     xlim = xlim, ylim = ylim)
+lines(c(-100,100), rep(0,2), col = 2, lwd = 2, lty = 2)
+lines(rep(0,2), c(-100,100), col = 2, lwd = 2, lty = 2)
+lines(c(-100,100), rep(mean_y,2), col = 1, lwd = 2, lty = 2)
+lines(rep(mean_x,2), c(-100,100), col = 1, lwd = 2, lty = 2)
+
+plot(x = eSVD_obj_adams$fit_Second$z_mat[,"Disease_Identity_IPF"][gene_names[de_other_idx]],
+     y = eSVD_obj_habermann$fit_Second$z_mat[,"Diagnosis_IPF"][gene_names[de_other_idx]],
+     xlab = "Adams", ylab = "Habermann", pch = 16, col = 4,
+     main = "DE other genes", asp = T,
+     xlim = xlim, ylim = ylim)
+lines(c(-100,100), rep(0,2), col = 2, lwd = 2, lty = 2)
+lines(rep(0,2), c(-100,100), col = 2, lwd = 2, lty = 2)
+lines(c(-100,100), rep(mean_y,2), col = 1, lwd = 2, lty = 2)
+lines(rep(mean_x,2), c(-100,100), col = 1, lwd = 2, lty = 2)
+
+plot(x = eSVD_obj_adams$fit_Second$z_mat[,"Disease_Identity_IPF"][gene_names[hk_idx]],
+     y = eSVD_obj_habermann$fit_Second$z_mat[,"Diagnosis_IPF"][gene_names[hk_idx]],
+     xlab = "Adams", ylab = "Habermann", pch = 16, col = 3,
+     main = "HK genes", asp = T,
+     xlim = xlim, ylim = ylim)
+lines(c(-100,100), rep(0,2), col = 2, lwd = 2, lty = 2)
+lines(rep(0,2), c(-100,100), col = 2, lwd = 2, lty = 2)
+lines(c(-100,100), rep(mean_y,2), col = 1, lwd = 2, lty = 2)
+lines(rep(mean_x,2), c(-100,100), col = 1, lwd = 2, lty = 2)
+
+plot(x = eSVD_obj_adams$fit_Second$z_mat[,"Disease_Identity_IPF"][gene_names[cycling_idx]],
+     y = eSVD_obj_habermann$fit_Second$z_mat[,"Diagnosis_IPF"][gene_names[cycling_idx]],
+     xlab = "Adams", ylab = "Habermann", pch = 16, col = 5,
+     main = "Cycling genes", asp = T,
      xlim = xlim, ylim = ylim)
 lines(c(-100,100), rep(0,2), col = 2, lwd = 2, lty = 2)
 lines(rep(0,2), c(-100,100), col = 2, lwd = 2, lty = 2)
--- main/habermann_T_esvd_postprocess.R
+++ main/habermann_T_esvd_postprocess.R
@@ -22,13 +22,21 @@ habermann_df_genes_others <- unique(unlist(lapply(file_vec, function(file_suffix
                      sep = ",")
   df_mat$X
 })))
+habermann_df_genes <- setdiff(habermann_df_genes, adams_df_genes)
 de_genes <- unique(c(adams_df_genes, habermann_df_genes))
+de_genes_others <- unique(c(adams_df_genes_others, habermann_df_genes_others))
 cycling_genes <- c(cc.genes$s.genes, cc.genes$g2m.genes)
+hk_genes <- read.csv("../../../data/housekeeping/housekeeping.txt", header = F)[,1]
+de_genes_others <- setdiff(de_genes_others, de_genes)
+cycling_genes <- setdiff(cycling_genes, c(de_genes_others, de_genes))
+hk_genes <- setdiff(hk_genes, c(cycling_genes, de_genes_others, de_genes))
 
 gene_names <- names(eSVD_obj$teststat_vec)
+adam_idx <- which(gene_names %in% adams_df_genes)
+habermann_idx <- which(gene_names %in% habermann_df_genes)
+de_other_idx <- which(gene_names %in% de_genes_others)
 cycling_idx <- which(gene_names %in% cycling_genes)
-de_idx <- which(gene_names %in% de_genes)
-
+hk_idx <- which(gene_names %in% hk_genes)
 
 #########################################
 
@@ -39,30 +47,42 @@ par(mfrow = c(2,2), mar = c(4,4,4,0.5))
 eSVD2:::gene_plot(eSVD_obj,
                   what_1 = "nuisance",
                   what_2 = "teststat",
-                  gene_list = list(gene_names[cycling_idx],
-                                   gene_names[de_idx]),
-                  color_palette = c(3,2))
+                  gene_list = list(gene_names[adam_idx],
+                                   gene_names[habermann_idx],
+                                   gene_names[de_other_idx],
+                                   gene_names[hk_idx],
+                                   gene_names[cycling_idx]),
+                  color_palette = c(1,2,4,3,5))
 
 eSVD2:::gene_plot(eSVD_obj,
                   what_1 = "nuisance",
-                  what_2 = "sparsity",
-                  gene_list = list(gene_names[cycling_idx],
-                                   gene_names[de_idx]),
-                  color_palette = c(3,2))
+                  what_2 = "Log_UMI",
+                  gene_list = list(gene_names[adam_idx],
+                                   gene_names[habermann_idx],
+                                   gene_names[de_other_idx],
+                                   gene_names[hk_idx],
+                                   gene_names[cycling_idx]),
+                  color_palette = c(1,2,4,3,5))
 
 eSVD2:::gene_plot(eSVD_obj,
                   what_1 = "Diagnosis_IPF",
                   what_2 = "teststat",
-                  gene_list = list(gene_names[cycling_idx],
-                                   gene_names[de_idx]),
-                  color_palette = c(3,2))
+                  gene_list = list(gene_names[adam_idx],
+                                   gene_names[habermann_idx],
+                                   gene_names[de_other_idx],
+                                   gene_names[hk_idx],
+                                   gene_names[cycling_idx]),
+                  color_palette = c(1,2,4,3,5))
 
 eSVD2:::gene_plot(eSVD_obj,
                   what_1 = "sparsity",
                   what_2 = "teststat",
-                  gene_list = list(gene_names[cycling_idx],
-                                   gene_names[de_idx]),
-                  color_palette = c(3,2))
+                  gene_list = list(gene_names[adam_idx],
+                                   gene_names[habermann_idx],
+                                   gene_names[de_other_idx],
+                                   gene_names[hk_idx],
+                                   gene_names[cycling_idx]),
+                  color_palette = c(1,2,4,3,5))
 
 graphics.off()
 
@@ -70,13 +90,30 @@ graphics.off()
 max_val <- max(abs(eSVD_obj$teststat_vec))
 break_vec <- seq(-max_val-0.15, max_val+0.15, by = 0.1)
 png(paste0("../../../out/fig/main/habermann_T_diagnostic_gene_histogram.png"),
-    height = 1500, width = 2500,
+    height = 2000, width = 3000,
     units = "px", res = 300)
+par(mfrow = c(2,3))
 eSVD2:::gene_plot(eSVD_obj,
                   what_1 = "teststat",
                   breaks = break_vec,
-                  gene_list = list(gene_names[cycling_idx],
-                                   gene_names[de_idx]),
-                  color_palette = c(3,2))
+                  gene_list = list(gene_names[adam_idx],
+                                   gene_names[habermann_idx],
+                                   gene_names[de_other_idx],
+                                   gene_names[hk_idx],
+                                   gene_names[cycling_idx]),
+                  color_palette = c(1,2,4,3,5))
+
+uniq_col_vec <- c(1,2,4,3,5)
+idx_list <- list(adam_idx, habermann_idx, de_other_idx, hk_idx, cycling_idx)
+names(idx_list) <- c("Adams", "Habermann", "Other DE", "HK", "Cell-cycle")
+for(kk in 1:length(idx_list)){
+  idx <- idx_list[[kk]]
+  hist(eSVD_obj$teststat_vec[idx], breaks = break_vec,
+       xlim = c(-max_val, max_val),
+       main = names(idx_list)[kk],
+       xlab = "teststat", ylab = "Frequency", freq = T)
+  lines(rep(median(eSVD_obj$teststat_vec),2), c(0, 1e5), lwd = 5, lty = 2, col = 2)
+  rug(eSVD_obj$teststat_vec[idx], col = uniq_col_vec[kk], lwd = 2)
+}
 graphics.off()
 
