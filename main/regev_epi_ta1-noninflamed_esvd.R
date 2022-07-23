08d368f6b22e29a18dddda77b2ac3d617463238c
linnykos <klin1234@gmail.com>
2022-07-23 12:27:47 -0400

    Pushing new experiment where for Regev, no samples are shared between analyses

--- main/regev_epi_ta1-noninflamed_esvd.R
+++ main/regev_epi_ta1-noninflamed_esvd.R
@@ -1,8 +1,10 @@
 rm(list=ls())
-load("../../../out/main/regevEpi_ta1_preprocessed.RData")
-
 library(Seurat)
 library(eSVD2)
+
+load("../../../out/main/regevEpi_ta1_preprocessed.RData")
+# table(regevEpi$Subject, regevEpi$Sample_Health)
+# table(regevEpi$Subject, regevEpi$Subject_Disease)
 
 set.seed(10)
 date_of_run <- Sys.time()
@@ -12,6 +14,23 @@ keep_vec <- rep(1, ncol(regevEpi))
 keep_vec[which(regevEpi$Sample_Health == "Inflamed")] <- 0
 regevEpi$keep <- keep_vec
 regevEpi <- subset(regevEpi, keep == 1)
+
+# take only half of the healthy subjects
+tab <- table(regevEpi$Subject, regevEpi$Subject_Disease)
+healthy_subj <- rownames(tab[tab[,"HC"] != 0,])
+set.seed(10)
+split1 <- sample(healthy_subj, size = round(length(healthy_subj)/2), replace = F)
+split2 <- setdiff(healthy_subj, split1)
+keep_vec <- rep(1, ncol(regevEpi))
+# Non-inflamed analysis uses split1, Inflamed uses split2
+if(any(regevEpi$Sample_Health == "Non-inflamed")){
+  keep_vec[which(regevEpi$Subject %in% split2)] <- 0
+} else {
+  keep_vec[which(regevEpi$Subject %in% split1)] <- 0
+}
+regevEpi$keep <- keep_vec
+regevEpi <- subset(regevEpi, keep == 1)
+
 regevEpi[["percent.mt"]] <- Seurat::PercentageFeatureSet(regevEpi, pattern = "^MT-")
 
 ##########
