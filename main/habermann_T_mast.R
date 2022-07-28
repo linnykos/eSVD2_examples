07616a41a7aa8660ebb0a09d5400082cde27b653
linnykos <klin1234@gmail.com>
2022-07-27 20:33:37 -0400

    Removing Tobacco from Habermann MAST to see if that'll help it run

--- main/habermann_T_mast.R
+++ main/habermann_T_mast.R
@@ -42,7 +42,7 @@ CD$cngeneson <- CD$ngeneson-mean(ngeneson)
 SummarizedExperiment::colData(sca) <- CD
 
 set.seed(10)
-mast_res <- MAST::zlm(formula = ~ grp.Diagnosis + cngeneson + grp.Gender + grp.Tobacco + grp.Age + grp.percent.mt,
+mast_res <- MAST::zlm(formula = ~ grp.Diagnosis + (1 | grp.Sample_Name) + cngeneson + grp.Gender + grp.Age + grp.percent.mt,
                       sca = sca,
                       method = "glmer",
                       ebayes = FALSE,
