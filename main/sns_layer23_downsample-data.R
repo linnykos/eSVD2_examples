1b2ab0900734fedfc27be77468274e63dae97afc
linnykos <klin1234@gmail.com>
2022-08-17 01:33:00 -0400

    Starting downsampled experiment

---
+++ main/sns_layer23_downsample-data.R
@@ -0,0 +1,28 @@
+rm(list=ls())
+library(Seurat)
+library(eSVD2)
+
+load("../../../out/main/sns_layer23_processed.RData")
+
+set.seed(10)
+date_of_run <- Sys.time()
+session_info <- devtools::session_info()
+
+##########
+
+mat <- Matrix::t(sns[["RNA"]]@counts[sns[["RNA"]]@var.features,])
+
+downsample_values <- c(0.9, 0.8, 0.7, 0.6, 0.5)
+for(downsample_value in downsample_values){
+  print(downsample_value)
+  x_vec <- mat@x
+
+  set.seed(10)
+  x_vec2 <- sapply(x_vec, function(x){
+    stats::rbinom(1, size = x, prob = downsample_value)
+  })
+
+  mat@x <- x_vec2
+  save(mat, date_of_run, session_info,
+       file = paste0("../../../out/main/sns_layer23_processed_downsampled-", downsample_value, ".RData"))
+}
---
+++ main/sns_layer23_esvd_volcano.R
@@ -0,0 +1,123 @@
+rm(list=ls())
+library(Seurat)
+library(eSVD2)
+
+load("../../../out/main/sns_layer23_esvd.RData")
+
+set.seed(10)
+date_of_run <- Sys.time()
+session_info <- devtools::session_info()
+
+#################
+
+df_vec <- eSVD2:::compute_df(input_obj = eSVD_obj,
+                             metadata = sns@meta.data,
+                             covariate_individual = "individual")
+teststat_vec <- eSVD_obj$teststat_vec
+p <- length(teststat_vec)
+gaussian_teststat <- sapply(1:p, function(j){
+  qnorm(pt(teststat_vec[j], df = df_vec[j]))
+})
+
+locfdr_res <- locfdr::locfdr(gaussian_teststat, plot = 0)
+fdr_vec <- locfdr_res$fdr
+names(fdr_vec) <- names(gaussian_teststat)
+null_mean <- locfdr_res$fp0["mlest", "delta"]
+null_sd <- locfdr_res$fp0["mlest", "sigma"]
+logpvalue_vec <- sapply(gaussian_teststat, function(x){
+  if(x < null_mean) {
+    Rmpfr::pnorm(x, mean = null_mean, sd = null_sd, log.p = T)
+  } else {
+    Rmpfr::pnorm(null_mean - (x-null_mean), mean = null_mean, sd = null_sd, log.p = T)
+  }
+})
+logpvalue_vec <- -(logpvalue_vec/log10(exp(1)) + log10(2))
+
+selected_genes <- names(fdr_vec)[which(fdr_vec <= 0.05)]
+
+############
+
+load("../../../data/sns_autism/velmeshev_genes.RData")
+tmp <- velmeshev_de_gene_df_list[[1]]
+tmp <- tmp[which(tmp[,"Cell type"] == "L2/3"),]
+de_gene_specific <- tmp[,"Gene name"]
+de_genes1 <- velmeshev_marker_gene_df[,"Gene name"]
+de_genes2 <- unlist(lapply(velmeshev_de_gene_df_list[-1], function(de_mat){
+  idx <- ifelse("Gene name" %in% colnames(de_mat), "Gene name", "HGNC Symbol")
+  de_mat[,idx]
+}))
+de_genes <- sort(unique(c(de_genes1, de_genes2)))
+de_genes <- de_genes[!de_genes %in% de_gene_specific]
+hk_genes <- read.csv("../../../data/housekeeping/housekeeping.txt", header = F)[,1]
+sfari_genes <- read.csv("../../../data/SFARI/SFARI-Gene_genes_09-02-2021release_01-06-2022export.csv", header = T)[,2]
+cycling_genes <- c(cc.genes$s.genes, cc.genes$g2m.genes)
+deg_df <- readxl::read_xlsx(
+  path = "../../../data/bulkRNA-DEG-autism/SupplementaryTable3.xlsx",
+  sheet = "DEGene_Statistics"
+)
+deg_df <- as.data.frame(deg_df)
+bulk_de_genes <- deg_df[which(deg_df[,"WholeCortex_ASD_FDR"]<=0.001),"external_gene_name"]
+length(bulk_de_genes)
+length(intersect(deg_df[,"external_gene_name"], colnames(eSVD_obj$dat)))
+
+length(intersect(selected_genes, de_gene_specific))
+length(intersect(selected_genes, de_genes))
+length(intersect(selected_genes, hk_genes))
+length(intersect(selected_genes, sfari_genes))
+length(intersect(selected_genes, cycling_genes))
+length(intersect(selected_genes, bulk_de_genes))
+
+quantile(logpvalue_vec[intersect(sfari_genes, colnames(eSVD_obj$dat))])
+quantile(logpvalue_vec[intersect(deg_df[,"external_gene_name"], colnames(eSVD_obj$dat))])
+
+#######
+
+idx <- which(names(eSVD_obj$teststat_vec) %in% selected_genes)
+safari_idx <- which(names(eSVD_obj$teststat_vec) %in% sfari_genes)
+hk_idx <- which(names(eSVD_obj$teststat_vec) %in% hk_genes)
+
+x_vec <- log2(eSVD_obj$case_mean) - log2(eSVD_obj$control)
+xlim <- quantile(x_vec, probs = c(0.01, 1))
+xlim <- c(-1,1)*max(abs(xlim))
+# y_vec <- abs(eSVD_obj$teststat_vec)
+y_vec <- logpvalue_vec
+ylim <- range(y_vec)
+
+png("../../../out/fig/main/sns_layer23_volcano.png",
+    height = 2500, width = 2500,
+    units = "px", res = 500)
+par(mar = c(3,3,0.4,0.1))
+plot(x = x_vec, y = y_vec,
+     xaxt = "n", yaxt = "n", bty = "n",
+     ylim = ylim, xlim = xlim,
+     cex.lab = 1.25, type = "n")
+for(j in seq(0,40,by = 2)){
+  lines(x = c(-1e4,1e4), y = rep(j, 2), col = "gray", lty = 2, lwd = 1)
+}
+points(x = x_vec, y = y_vec,
+       col = rgb(0.6, 0.6, 0.6, 0.3), pch = 16)
+points(x = x_vec[idx], y = y_vec[idx],
+       col = 2, pch = 16, cex = 1.5)
+points(x = x_vec[setdiff(safari_idx, idx)], y = y_vec[setdiff(safari_idx, idx)],
+       col = 3, pch = 16, cex = 1, lwd = 2)
+points(x = x_vec[hk_idx], y = y_vec[hk_idx],
+       col = "white", pch = 16, cex = 1)
+points(x = x_vec[hk_idx], y = y_vec[hk_idx],
+       col = rgb(34, 151, 230, 255*.35, maxColorValue = 255), pch = 16, cex = 1)
+points(x = x_vec[intersect(idx, safari_idx)], y = y_vec[intersect(idx, safari_idx)],
+       col = "white", pch = 1, cex = 2, lwd = 3)
+points(x = x_vec[intersect(idx, safari_idx)], y = y_vec[intersect(idx, safari_idx)],
+       col = 3, pch = 1, cex = 2, lwd = 2)
+axis(1, cex.axis = 1.25, cex.lab = 1.25, lwd = 2)
+axis(2, cex.axis = 1.25, cex.lab = 1.25, lwd = 2)
+lines(x = rep(0, 2), y = c(-10,100), lwd = 1.5, lty = 3, col = 1)
+graphics.off()
+
+
+
+
+
+
+
+
+
