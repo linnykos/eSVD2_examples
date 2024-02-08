140de5d18e03d9c5022df173a9688f8dc6c1b1d8
Kevin Lin <kzlin@uw.edu>
2024-02-08 01:15:50 -0800

    Pushing the shell script

---
+++ simulation/simulation_power_mast_all-combinations.sh
@@ -0,0 +1,9 @@
+sbatch simulation_power_mast_all-combinations11.slurm
+sbatch simulation_power_mast_all-combinations12.slurm
+sbatch simulation_power_mast_all-combinations13.slurm
+sbatch simulation_power_mast_all-combinations21.slurm
+sbatch simulation_power_mast_all-combinations22.slurm
+sbatch simulation_power_mast_all-combinations23.slurm
+sbatch simulation_power_mast_all-combinations31.slurm
+sbatch simulation_power_mast_all-combinations32.slurm
+sbatch simulation_power_mast_all-combinations33.slurm
